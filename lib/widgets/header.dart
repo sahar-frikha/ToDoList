d2c53c0f3a0167f9001f8deb460e11c669f020bd
Sahar FRIKHA <sahar.frikha@etudiant-enit.utm.tn>
2020-06-10 00:53:26 +0100

    My first commit

---
+++ lib/widgets/header.dart
@@ -0,0 +1,27 @@
+/**
+ * Created by Sahar FRIKHA
+ * https://github.com/sahar-frikha
+ */
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  final String msg;
+
+  Header({this.msg});
+
+  @override
+  Widget build(BuildContext context) {
+    return LimitedBox(
+      child: Container(
+        padding: EdgeInsets.only(left: 15.0, top: 30.0),
+        child: Text(
+          msg,
+          style: Theme.of(context)
+              .textTheme
+              .headline
+              .copyWith(fontFamily: 'Lobster Two', fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
